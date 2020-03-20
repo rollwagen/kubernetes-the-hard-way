0367c354814de4f1b161f0bbff81c5dbac15ef51
rollwagen <rollwagen@gmail.com>
2020-03-20 17:48:38 +0100

    Fixed to no-prompt non-interactive update.

--- update-os-hosts.sh
+++ update-os-hosts.sh
@@ -1,4 +1,4 @@
 
 for instance in controller-0 controller-1 controller-2 worker-0 worker-1 worker-2; do
-	vagrant ssh ${instance} -- "sudo apt-get update;sudo apt-get upgrade -y;sudo apt autoremove -y"
+	vagrant ssh ${instance} -- "sudo apt-get update; sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade;"
 done
