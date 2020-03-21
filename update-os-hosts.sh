1429f9cf5ca27cfb68978ab71a89bf7a3dab9276
rollwagen <rollwagen@gmail.com>
2020-03-21 11:36:28 +0100

    Added color output and 'apt autoremove'.

--- update-os-hosts.sh
+++ update-os-hosts.sh
@@ -1,4 +1,5 @@
 
 for instance in controller-0 controller-1 controller-2 worker-0 worker-1 worker-2; do
-	vagrant ssh ${instance} -- "sudo apt-get update; sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade;"
+	printf "[\e[1m\e[33mINFO\e[0m] Updating \e[1m${instance}\e[0m...\n"
+	vagrant ssh ${instance} -- "sudo apt-get update; sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade; sudo apt autoremove -y;"
 done
